1dd57ca381ff7af72f6496e96ee6aa518e37b6fc
Amanda Andrade <amanda.andradetec@gmail.com>
2025-11-21 14:00:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Botão "voltar ao topo"
+const scrollTopBtn = document.querySelector('.scroll-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollTopBtn.classList.add('visible');
+  } else {
+    scrollTopBtn.classList.remove('visible');
+  }
+});
+
+scrollTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
